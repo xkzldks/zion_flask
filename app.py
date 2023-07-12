3a3daeed3298d93474ecb688717bd26710bfb753
xkzldks <74147023+xkzldks@users.noreply.github.com>
2023-07-13 01:10:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template
+import time
+import os
+import account
+from random import *
+from flask import Flask, url_for, render_template, request, redirect, session, jsonify, flash, send_file
+from datetime import datetime
+from model2 import db
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
