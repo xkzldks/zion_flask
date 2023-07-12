1a2815721b21e54b00db24b0106ee5b3f3e3d043
xkzldks <74147023+xkzldks@users.noreply.github.com>
2023-07-13 01:12:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ from datetime import datetime
 from model2 import db
 from bson.objectid import ObjectId
 
+now = datetime.now()
 app = Flask(__name__)
+
+# account app import
+app.register_blueprint(account.blue_account)
+
+# 파일 업로드 위치
+app.config['UPLOAD_FOLDER'] = 'static/upload/'
 
 @app.route("/")
 def index():
