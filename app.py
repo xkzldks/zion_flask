31f2f052fb400c303909c1f636e91a7ef0714ab0
xkzldks <74147023+xkzldks@users.noreply.github.com>
2023-07-13 00:45:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('./index.html')
+
+if __name__ == '__main__':
+    app.secret_key = "123"
+    app.run('0.0.0.0', debug=True)
