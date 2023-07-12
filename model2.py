138acc897076b770d2558cf0bb3fba48eef0d935
xkzldks <74147023+xkzldks@users.noreply.github.com>
2023-07-13 02:17:29 +0900

    Update model2.py

--- model2.py
+++ model2.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 # db
-client = MongoClient('mongodb://test:test@localhost', 27017)
+client = MongoClient('@mongodb://test:test@svc.sel4.cloudtype.app:32165/?authMechanism=DEFAULT', 27017)
 #client = MongoClient('localhost', 27017)
 db = client.dbsparta
