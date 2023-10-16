92e00a8cc2eabc223c8557ba7c3d6032a700f555
xkzldks <naochugu@gmail.com>
2023-10-17 05:54:16 +0900

    Update model2.py

--- model2.py
+++ model2.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 
 # db
+# client = MongoClient('mongodb://test:test@localhost', 27017)
+# client = MongoClient('localhost', 27017)
 client = MongoClient('mongodb://test:test@svc.sel4.cloudtype.app:32165/?authMechanism=DEFAULT', 27017)
-#client = MongoClient('localhost', 27017)
 db = client.dbsparta
+appSecret = "1!2@3#4$"
