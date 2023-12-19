b157310cd814dd2689f4a147761ac4b4f2e5ec63
xkzldks <naochugu@gmail.com>
2023-12-19 15:13:47 +0900

    y

--- static/notitest.js
+++ static/notitest.js
@@ -13,14 +13,14 @@ function makeNoti() {
     alert("알림이 차단된 상태입니다. 알림 권한을 허용해주세요.");
   } else {
 
-    let notification = new Notification("test", { // "test" => 제목
+    let notification = new Notification("시온출석체크", { // "test" => 제목
       body: day +'일자 출석이 기록되었습니다. 확인해주세요', // 메세지, ####-##-##일자 출석이 기록되었습니다. ####-##-##일자 출석인원이 추가되었습니다.
       icon: `/lib/img/novalogo_copy.png`, // 아이콘
     });
 
     //알림 클릭 시 이벤트
     notification.addEventListener("click", () => {
-      window.open('zion2024.site');
+      window.open('https://zion2024.site/');
     });
 
   }
