50131e59017cbda1307a07c4e592d97e168aee1b
xkzldks <74147023+xkzldks@users.noreply.github.com>
2024-11-23 15:35:38 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -1250,7 +1250,8 @@ def autoSave():
     for i in mailList:
         print(i)
         message = EmailMessage()
-        message.set_content(result + '\n\n\nhttp://zion' + now.date().strftime("%Y") + ".site\n")  # 내용
+        message.set_content(result + '\n\n\nhttp://zion' + now.date().strftime("%Y") + ".site\n"
++"https://port-0-zion-flask-k19y2kljzuim4t.sel4.cloudtype.app/")  # 내용
         message["Subject"] = str("[진주교회 시온청년부] " + now.date().strftime("%Y/%m/%d") + " 출석백업")  # 제목
         message["From"] = EMAIL_ADDR  # 보내는 사람의 이메일 계정
         message["To"] = i['메일주소']  # 받는 사람
